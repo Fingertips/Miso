cdc29ad08596995a3071f8fb055782486c055a0c
Eloy Duran <eloy.de.enige@gmail.com>
2009-10-20 22:01:56 +0200

    Crop an image with CoreImage.

--- lib/miso/processor/core_image.rb
+++ lib/miso/processor/core_image.rb
@@ -8,11 +8,49 @@ module Miso
       def initialize(input_file)
         super
         require 'osx/cocoa'
-        @image = OSX::CIImage.imageWithContentsOfURL(OSX::NSURL.fileURLWithPath(input_file))
+        reset!
       end
       
       def dimensions
-        @image.extent.size.to_a
+        @dimensions ||= ci_image.extent.size.to_a
+      end
+      
+      def crop(width, height)
+        apply_filter 'CICrop', 'inputRectangle' => vector(width, height)
+      end
+      
+      def write(output_file)
+        bitmap = OSX::NSBitmapImageRep.alloc.initWithCIImage(@buffer)
+        blob = bitmap.representationUsingType_properties(detect_file_type(output_file), nil)
+        blob.writeToFile_atomically(output_file, false)
+        reset!
+      end
+      
+      private
+      
+      def reset!
+        @buffer = ci_image
+      end
+      
+      def ci_image
+        OSX::CIImage.imageWithContentsOfURL(OSX::NSURL.fileURLWithPath(@input_file))
+      end
+      
+      def vector(width, height)
+        OSX::CIVector.vectorWithX_Y_Z_W(0, 0, width, height)
+      end
+      
+      def apply_filter(name, options)
+        filter = OSX::CIFilter.filterWithName(name)
+        filter.setDefaults
+        options.merge('inputImage' => @buffer).each do |name, value|
+          filter.setValue_forKey(value, name)
+        end
+        @buffer = filter.valueForKey('outputImage')
+      end
+      
+      def detect_file_type(path)
+        OSX::NSPNGFileType
       end
     end
   end
--- spec/api/processor/core_image_spec.rb
+++ spec/api/processor/core_image_spec.rb
@@ -1,36 +1,43 @@
 require File.expand_path('../../../start', __FILE__)
 
-describe "Miso::Processor::CoreImage" do
-  it "should check the load paths to see if RubyCocoa is available" do
-    Miso::Processor::CoreImage.should.be.available
-    with_load_path '/tmp', '~/' do
-      Miso::Processor::CoreImage.should.not.be.available
+begin
+  require 'osx/cocoa'
+  
+  describe "Miso::Processor::CoreImage" do
+    it "should check the load paths to see if RubyCocoa is available" do
+      Miso::Processor::CoreImage.should.be.available
+      with_load_path '/tmp', '~/' do
+        Miso::Processor::CoreImage.should.not.be.available
+      end
+    end
+    
+    it "should require osx/cocoa on initialization" do
+      Miso::Processor::CoreImage.any_instance.expects(:require).with('osx/cocoa')
+      Miso::Processor::CoreImage.new(fixture_file('120x100.png'))
     end
   end
   
-  it "should require osx/cocoa on initialization" do
-    Miso::Processor::CoreImage.any_instance.expects(:require).with('osx/cocoa')
-    Miso::Processor::CoreImage.new(fixture_file('120x100.png'))
-  end
-end
-
-describe "An instance of Miso::Processor::CoreImage" do
-  before do
-    @image_120_x_100 = Miso::Image.new(fixture_file('120x100.png'), Miso::Processor::CoreImage)
-    @output_file = temp_file('temp.png')
+  describe "An instance of Miso::Processor::CoreImage" do
+    before do
+      @image_120_x_100 = Miso::Image.new(fixture_file('120x100.png'), Miso::Processor::CoreImage)
+      @output_file = temp_file('temp.png')
+    end
+    
+    it "should crop to specified dimensions" do
+      @image_120_x_100.crop(40, 30).write(@output_file).dimensions.should == [40, 30]
+      @image_120_x_100.crop(40, 33).write(@output_file).dimensions.should == [40, 33]
+    end
+    
+    it "should fit to specified dimensions, conserving the original aspect ratio" do
+      @image_120_x_100.fit(40, 30).write(@output_file).dimensions.should == [36, 30]
+      @image_120_x_100.fit(40, 34).write(@output_file).dimensions.should == [40, 33]
+    end
+    
+    it "should return its dimensions" do
+      @image_120_x_100.dimensions.should == [120, 100]
+    end
   end
   
-  it "should crop to specified dimensions" do
-    @image_120_x_100.crop(40, 30).write(@output_file).dimensions.should == [40, 30]
-    @image_120_x_100.crop(40, 33).write(@output_file).dimensions.should == [40, 33]
-  end
-  
-  it "should fit to specified dimensions, conserving the original aspect ratio" do
-    @image_120_x_100.fit(40, 30).write(@output_file).dimensions.should == [36, 30]
-    @image_120_x_100.fit(40, 34).write(@output_file).dimensions.should == [40, 33]
-  end
-  
-  it "should return its dimensions" do
-    @image_120_x_100.dimensions.should == [120, 100]
-  end
+rescue LoadError
+  warn "[!] Skipping Miso::Processor::CoreImage spec."
 end
